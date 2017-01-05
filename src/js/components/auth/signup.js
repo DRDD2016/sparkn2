614dad14b2aa99b0e889eca4f70e41276cd9c287
minaorangina <wilhelmina.jane@gmail.com>
2017-01-05 13:00:29 +0000

    adds firstname and surname fields to signup form #62

--- src/js/components/auth/signup.js
+++ src/js/components/auth/signup.js
@@ -9,6 +9,16 @@ function Signup ({ handleSubmit, handleSubmitForm }) {
 
   return (
     <View style={{ marginTop: 50 }}>
+      <Text style={{ paddingLeft: 5 }}>First name</Text>
+      <View style={ styles.row }>
+        <Field name="firstname" component={ FormTextInput } />
+      </View>
+
+      <Text style={{ paddingLeft: 5 }}>Surname</Text>
+      <View style={ styles.row }>
+        <Field name="surname" component={ FormTextInput } />
+      </View>
+
       <Text style={{ paddingLeft: 5 }}>Email</Text>
       <View style={ styles.row }>
         <Field name="email" component={ FormTextInput } />
