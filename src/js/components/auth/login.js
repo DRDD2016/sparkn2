bb88a8350779c34ec138de17ae8f48d66356f75a
minaorangina <wilhelmina.jane@gmail.com>
2017-01-05 13:06:52 +0000

    connects login view with redux-form #62

--- src/js/components/auth/login.js
+++ src/js/components/auth/login.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TouchableHighlight, View } from 'react-native';
+import { Field, reduxForm } from 'redux-form';
+import { FormTextInput, FormPasswordInput } from './form-components';
 import Router from '../../router';
-import Button from '../common/Button';
 import styles from '../../../styles';
 
 
-export default function Login ({ navigator }) {
+function Login ({ navigator, handleSubmit, handleSubmitForm }) {
 
   const goToNavbar = () => {
     navigator.push(Router.getRoute('navbar'));
@@ -15,28 +16,21 @@ export default function Login ({ navigator }) {
     <View style={{ marginTop: 50 }}>
       <Text style={{ paddingLeft: 5 }}>Email</Text>
       <View style={ styles.row }>
-        <TextInput
-          style={ styles.inputStyle }
-          onChangeText={ text => console.log(text) }
-          value={ 'text' }
-          type="text"
-        />
+        <Field name="email" component={ FormTextInput } />
       </View>
 
       <Text style={{ paddingLeft: 5 }}>Password</Text>
       <View style={ styles.row }>
-        <TextInput
-          style={ styles.inputStyle }
-          onChangeText={ text => console.log(text) }
-          value={ 'text' }
-          type="password"
-          secureTextEntry
-        />
+        <Field name="password" component={ FormPasswordInput } />
       </View>
 
-      <Button textStyle={ styles.buttonTextStyle } buttonStyle={ styles.buttonStyle } onPress={ goToNavbar }>
-        Login
-      </Button>
+      <TouchableHighlight
+        onPress={handleSubmit(handleSubmitForm)}
+      >
+        <Text>SIGN UP</Text>
+      </TouchableHighlight>
     </View>
   );
 }
+
+export default reduxForm({ form: 'login' })(Login);
--- src/js/containers/auth/login.js
+++ src/js/containers/auth/login.js
@@ -1,4 +1,10 @@
 import { connect } from 'react-redux';
 import Login from '../../components/auth/login';
 
-export default connect()(Login);
+const mapDispatchToProps = () => ({
+  handleSubmitForm: (values, dispatch, props) => {
+    console.log(values, dispatch, props);
+  }
+});
+
+export default connect(mapDispatchToProps)(Login);
